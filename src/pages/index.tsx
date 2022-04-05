0df3fe0bb02df80090758d43160c3980eda4ecc9
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-04-05 13:20:30 -0700

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,15 +11,15 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <h1 className="hero__title">{siteConfig.tagline}</h1>
+        <p className="hero__subtitle">PCC Archive is build by the community and for the community of Purrnelope's Country Club</p>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/">
             Home
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
@@ -29,11 +29,11 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to ${siteConfig.title} #WeLoveThePurrs`}
+      description="Wiki and post archive for Purrnelope's Country Club.">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
