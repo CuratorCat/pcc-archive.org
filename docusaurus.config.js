b76d1f5a6f34eeefa3849e3e2e298ee5317a9137
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-04-05 10:07:57 -0700

    use @cmfcmf/docusaurus-search-local

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,6 +120,26 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  
+  plugins: [  
+    [require.resolve('@cmfcmf/docusaurus-search-local'), {
+      indexDocs: true,
+      indexDocSidebarParentCategories: 3,
+      indexBlog: true,
+      indexPages: true,
+      language: ["en"],
+      style: undefined,
+      lunr: {
+        tokenizerSeparator: /[\s\-]+/,
+        // https://lunrjs.com/guides/customising.html#similarity-tuning
+        b: 0.75,
+        k1: 1.2,
+        titleBoost: 5,
+        contentBoost: 1,
+        parentCategoriesBoost: 2,
+      }
+    }]
+  ],
 };
 
 module.exports = config;
