fc871e9838fde77d2b3ab5d728c27141ceb500ca
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-04-09 01:14:08 -0700

    deps: add tailwindcss

--- config/plugin.js
+++ config/plugin.js
@@ -20,4 +20,15 @@ module.exports = [
       },
     },
   ],
+  async function myPlugin(context, options) {
+    return {
+      name: "docusaurus-tailwindcss",
+      configurePostCss(postcssOptions) {
+        // Appends TailwindCSS and AutoPrefixer.
+        postcssOptions.plugins.push(require("tailwindcss"));
+        postcssOptions.plugins.push(require("autoprefixer"));
+        return postcssOptions;
+      },
+    };
+  },
 ];
