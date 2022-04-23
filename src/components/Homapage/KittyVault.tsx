d1cbbade33a5534f713ee8d55b4507382b527fb6
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-04-23 05:33:30 -0700

    add component: home/KittyVault

---
+++ src/components/Homapage/KittyVault.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+
+export default function KittyVault() {
+  return (
+    <section className="relative my-8 md:my-12">
+      <div className="relative max-w-xl mx-auto px-8 md:max-w-5xl ">
+        <div className="relative sm:grid md:grid-cols-2 lg:gap-8 lg:items-center bg-slate-500/10 hover:bg-slate-500/20 rounded-3xl px-8 py-8 md:py-12 group transition-all duration-300">
+          <div
+            className="mb-4 md:mb-0 relative lg:mt-0 items-center"
+            aria-hidden="true"
+          >
+            <img
+              className="relative mx-auto my-auto w-[200px] sm:w-[250px] md:w-[300px] lg:w-[350px] xl:w-[400px] group-hover:animate-pulse"
+              src="/img/kittyvault-s.png"
+              alt=""
+            />
+          </div>
+
+          <div className="relative">
+            <h3 className="text-center md:text-left text-3xl md:text-5xl uppercase pb-4">
+              Kitty Vault
+            </h3>
+            <p className="mt-6 text-center md:text-left text-base sm:text-xl lg:text-lg xl:text-xl">
+              The Kitty Vault is a vault of NFTs that backs every PCC Cats,
+              Kittens, companion 2 and main airdrops. The idea is that the Vault
+              will operate as a 'Gold Standard' so PCC collections will always
+              have some inherent worth.
+            </p>
+            <div className="block pt-8 pb-4 md:pb-0 text-center md:text-left">
+              <Link to="/kittyvault">
+                <span className="px-6 py-3 rounded-full bg-pink-500 hover:bg-white hover:shadow-lg hover:shadow-pink-500 text-base font-medium text-white hover:text-pink-500 transition-all duration-300">
+                  Learn More
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
