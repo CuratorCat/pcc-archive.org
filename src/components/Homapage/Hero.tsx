944868aed53574de11b6c7b5daba6f11f28b0bda
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-08-04 09:40:01 -0700

    update home contribute text

--- src/components/Homapage/Hero.tsx
+++ src/components/Homapage/Hero.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
       <div className="relative sm:overflow-hidden">
         <div className="relative px-4 py-4 md:py-8 sm:px-6 lg:px-8 ">
           <div className="mx-auto max-w-7xl px-4">
-            <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-              <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+            <div className="lg:grid lg:grid-cols-12">
+              <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-8 lg:max-w-3xl lg:text-left">
                 <h1>
                   <span className="block text-base uppercase tracking-wide sm:text-xl xl:text-3xl ">
                     WELCOME TO
@@ -17,9 +17,11 @@ export default function Hero() {
                   </span>
                 </h1>
                 <p className="pt-1 text-base sm:pt-3 sm:text-xl lg:text-lg xl:text-xl">
-                  PCC Archive is created for people to learn and find
-                  information about the Purrnelope's Country Club project.
-                  Welcome to{" "}
+                  PCC Archive is created for people to learn about the
+                  Purrnelope's Country Club project.
+                </p>
+                <p className="pt-1 text-base sm:pt-3 sm:text-xl lg:text-lg xl:text-xl">
+                  Anyone is welcomed to{" "}
                   <a href="/contribute">
                     <span className="underline hover:text-violet-500">
                       contribute to the content
