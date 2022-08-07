4c75eaf47c650fde1c94099c6ba410c04a1940b9
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-08-07 10:09:05 -0700

    fix Home Hero padding on mobile

--- src/components/Homapage/Hero.tsx
+++ src/components/Homapage/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="relative">
       <div className="relative sm:overflow-hidden">
-        <div className="relative px-4 py-4 md:py-8 sm:px-6 lg:px-8 ">
+        <div className="relative px-0 py-4 md:py-8 sm:px-6 lg:px-8 ">
           <div className="mx-auto max-w-7xl px-4">
             <div className="lg:grid lg:grid-cols-12">
               <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-8 lg:max-w-3xl lg:text-left">
