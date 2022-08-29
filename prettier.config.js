bd5b9543c3a61486984bb61faaf3b2d0abdd2beb
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-08-29 08:13:16 -0700

    add deps: prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  semi: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 120,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  tabWidth: 2,
+}
