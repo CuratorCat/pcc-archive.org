c33690915e35fbe5bb824c1ddb00d58fa7e6a64b
Tejvir Mann <tejvirmann@Tejvirs-MacBook-Pro.local>
2024-02-09 23:11:30 -0600

    hih

--- src/pages/resources/_components/ShowcaseCard/index.tsx
+++ src/pages/resources/_components/ShowcaseCard/index.tsx
@@ -44,45 +44,47 @@ function ShowcaseCardTag({ tags }: { tags: TagType[] }) {
 
 function ShowcaseCard({ resource }: { resource: Resource }) {
   return (
-    <li
-      key={resource.title}
-      className={
-        'flex flex-col justify-between rounded-xl bg-white/50 sm:hover:scale-105 transition-all duration-150 dark:bg-black/50 border border-slate-500/10 dark:border-slate-500/0 overflow-hidden group ' +
-        styles.showcaseCard
-      }
-    >
-      <div>
-        <div className={styles.showcaseCardImage}>
-          <img
-            src={require('../../../../data/resources/img/' + resource.image).default}
-            alt={resource.title}
-            className="h-[150px] w-full object-cover"
-          />
-        </div>
-        <div className="px-3 py-1">
-          <div className={clsx(styles.showcaseCardHeader)}>
-            <h4 className={styles.showcaseCardTitle}>
-              <Link href={resource.website} className={styles.showcaseCardLink}>
-                {resource.title}
-              </Link>
-            </h4>
-            {resource.tags.includes('favorite') && '💜'}
-            {resource.source && (
-              <Link
-                href={resource.source}
-                className={clsx('button button--secondary button--sm', styles.showcaseCardSrcBtn)}
-              >
-                Source
-              </Link>
-            )}
+    <Link href={resource.website} className={styles.showcaseCardLink}>
+      <li
+        key={resource.title}
+        className={
+          'flex flex-col justify-between rounded-xl bg-white/50 sm:hover:scale-105 transition-all duration-150 dark:bg-black/50 border border-slate-500/10 dark:border-slate-500/0 overflow-hidden group ' +
+          styles.showcaseCard
+        }
+      >
+        <div>
+          <div className={styles.showcaseCardImage}>
+            <img
+              src={require('../../../../data/resources/img/' + resource.image).default}
+              alt={resource.title}
+              className="h-[200px] w-full object-cover"
+            />
           </div>
-          <p className={styles.showcaseCardBody}>{resource.description}</p>
+          <div className="px-3 py-1">
+            <div className={clsx(styles.showcaseCardHeader)}>
+              <h4 className={styles.showcaseCardTitle}>
+                <Link href={resource.website} className={styles.showcaseCardLink}>
+                  {resource.title}
+                </Link>
+              </h4>
+              {/* {resource.tags.includes('favorite') && '💜'} */}
+              {resource.source && (
+                <Link
+                  href={resource.website}
+                  className={clsx('button button--secondary button--sm', styles.showcaseCardSrcBtn)}
+                >
+                  Source
+                </Link>
+              )}
+            </div>
+            <p className={styles.showcaseCardBody}>{resource.description}</p>
+          </div>
         </div>
-      </div>
-      <ul className={` ${styles.cardFooter}`}>
-        <ShowcaseCardTag tags={resource.tags} />
-      </ul>
-    </li>
+        <ul className={` ${styles.cardFooter}`}>
+          <ShowcaseCardTag tags={resource.tags} />
+        </ul>
+      </li>
+    </Link>
   )
 }
 
